a85e5000747797edd81c81a66108ac16fd8cfb1b
Ryan McGeehan <billygoatmagoo@gmail.com>
2016-05-07 12:27:48 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#Anatomy of an Ethereum Address
+
+This script outputs the step by step creation of an Ethereum address.
+
+##Example Output
+
+1. Private Key (No Hex): 30594893532191419170483904360138267329943395403786937494850298450853451941303
+2. Private Key (Hex): 43A41898B13B5F29CA522121FCB32B169C62A3FB0CDA6D0FD90E33310AB245B7
+3. Public Key (Hex): 047CD62D1BACE6AB7ECB4620218903A30C33BC05F50522E96349E31780E8DE4E2E22950F3BD4CC2D5A2E7940CA700CA22265D9F477973322A2ED84E7E0E32605B8
+4. Strip leading pubkey byte: 7cd62d1bace6ab7ecb4620218903a30c33bc05f50522e96349e31780e8de4e2e22950f3bd4cc2d5a2e7940ca700ca22265d9f477973322a2ed84e7e0e32605b8
+4. Keccak-256 (Public Key): 4aac99bda2f7cdc1564d90f12407e85967f2c56298a3c5e51627a42240aa6536
+5. Last 20 Bytes: 2407e85967f2c56298a3c5e51627a42240aa6536
+6. Address: 0x2407e85967f2c56298a3c5e51627a42240aa6536
+
+##Dependencies
+See comments regarding the nebulous state of SHA3 and Ethereum. If the following gem is ever updated to the FIPS standard, this script will break.
+
+`gem install digest-sha3`
---
+++ make_address.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'openssl'
+require 'digest/sha3'
+
+include OpenSSL
+
+#Make sure you have the right hashing algoritm for Ethereum.
+#Ethereum does not use SHA3 despite its extensive documentation and reference in its codebase. It uses an early version of "Keccak"
+
+#This is confusing for three reasons: There is Keccak-256, The NIST FIPS 202 flavor of Keccak, and SHA3-256.
+#To test, the string "testing" should result in: 5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02
+#https://medium.com/@ConsenSys/are-you-really-using-sha-3-or-old-code-c5df31ad2b0#.c7zveoc1c
+#http://ethereum.stackexchange.com/questions/550/which-cryptographic-hash-function-does-ethereum-use
+
+#This has created a standards problem. 
+
+#The gem we use here incorrectly uses Keccak and says it's SHA3, just like Ethereum does. It's unmaintained, which means it will never be "corrected" which is exactly what we want. I don't like this either.
+
+digest = Digest::SHA3.new(256)
+
+#Generate underlying EC / secp256k1 key
+key = PKey::EC.new('secp256k1');
+key = key.generate_key
+
+#Printing Private Keys
+puts "1. Private Key (No Hex): #{key.private_key}"
+puts "2. Private Key (Hex): #{key.private_key.to_s(16)}"
+
+ecdsa_pubkey = key.public_key.to_bn.to_s(16)
+#Public Key Address.
+puts "3. Public Key (Hex): #{ecdsa_pubkey}"
+
+#The golang implementation strips the leading byte of the public key before hashing.
+#Documentation claims that the whole pubkey is hashed.
+#See: https://github.com/ethereum/go-ethereum/blob/master/crypto/crypto.go#L341
+#This step is also special because we are hashing a hex string, not a string, so we pack('H*') it.
+prehash_ecdsa_pubkey = [ecdsa_pubkey].pack('H*').slice(1,64)
+
+puts "4. Strip leading pubkey byte: #{prehash_ecdsa_pubkey.unpack('H*')[0]}"
+
+#Use the Keccak-256 hash on the hex string.
+keccak_hash = digest.hexdigest(prehash_ecdsa_pubkey)
+
+puts "4. Keccak-256 (Public Key): #{keccak_hash}"
+
+#The last 20 bytes of a Keccak-256 address is an actual Ethereum address.
+puts "5. Last 20 Bytes: #{keccak_hash.byteslice(24,40)}"
+
+#Format for Ethereum
+puts "6. Address: 0x#{keccak_hash.byteslice(24,40)}"
